915f236a0f6b0d8cba602af7b11727ccb2185bec
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-08-22 15:06:36 +0530

    first commit

---
+++ build/CMakeFiles/main.x.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.x.dir/main.cpp.o"
+  "CMakeFiles/main.x.dir/main.cpp.o.d"
+  "main.x"
+  "main.x.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
