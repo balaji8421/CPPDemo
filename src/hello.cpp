071465a73c21108efb5d9572fe90a84ccd255d26
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-08-30 11:35:45 +0530

    modified code

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "hello.h"
 
 void hello(){
     std::cout << "Hello, World!" << std::ends;
+    std::this_thread::sleep_for(std::chrono::hours(1));
 }
